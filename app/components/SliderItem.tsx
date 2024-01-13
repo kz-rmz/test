ca061bf6b51a3c4a400188c5269b25afa6357af2
karnurmax <karnurmax@mail.ru>
2024-01-13 18:30:35 +0600

    Fix: React ref type issue when deploy in Vercel

--- app/components/SliderItem.tsx
+++ app/components/SliderItem.tsx
@@ -13,26 +13,24 @@ type Props = {
   isActive?: boolean;
 };
 
-const SliderItem = forwardRef((props: Props, ref: React.ForwardedRef<HTMLInputElement>) => {
+const SliderItem = forwardRef((props: Props, ref: React.ForwardedRef<HTMLDivElement>) => {
   return (
-    <>
-      <div ref={ref} className={props.className}>
-        <img
-          src={props.slide.img}
-          alt={props.slide.title}
-          className={cn(
-            `block object-cover min-h-[240px] min-w-[320px]  lg:h-[344px] lg:min-w-[344px] ${props.className}`,
-            {
-              "min-h-[240px] min-w-[320px] lg:min-w-[688px]": props.slide.title?.length > 35,
-            }
-          )}
-        />
-        <p className={cn("text-lg md:text-[28px] text-[#525C7A] mt-8", { "text-emerald-500": props.isActive })}>
-          {props.slide.title}
-        </p>
-        <p className='text-md md:text-lg text-[#A2ACC7]'>{props.slide.date}</p>
-      </div>
-    </>
+    <div ref={ref} className={props.className}>
+      <img
+        src={props.slide.img}
+        alt={props.slide.title}
+        className={cn(
+          `block object-cover min-h-[240px] min-w-[320px]  lg:h-[344px] lg:min-w-[344px] ${props.className}`,
+          {
+            "min-h-[240px] min-w-[320px] lg:min-w-[688px]": props.slide.title?.length > 35,
+          }
+        )}
+      />
+      <p className={cn("text-lg md:text-[28px] text-[#525C7A] mt-8", { "text-emerald-500": props.isActive })}>
+        {props.slide.title}
+      </p>
+      <p className='text-md md:text-lg text-[#A2ACC7]'>{props.slide.date}</p>
+    </div>
   );
 });
 
